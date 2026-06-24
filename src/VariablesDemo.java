a7e35b0889e310857b0db9f616f4917f711e3a8c
ANJani9web <anjanison9@gmail.com>
2026-06-24 08:56:26 +0530

    Learn variables and Java execution flow

---
+++ src/VariablesDemo.java
@@ -0,0 +1,44 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        String name = "Anjani";
+        int age = 22;
+        double cgpa = 8.5;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(cgpa);
+
+        // output : Anjani
+        // 22
+        // 8.5
+
+        // Exercise 1 : Modify your code:
+        // String project = "Birthday Reminder";
+        // int contacts = 50;
+        // double version = 1.0; and print all them
+
+        // String project = "Birthday Reminder";
+        // int contacts = 50;
+        // double version = 1.0;
+
+        // System.out.println(project);
+        // System.out.println(contacts);
+        // System.out.println(version);
+
+        // output : Birthday Reminder
+        // 50
+        // 1.0
+
+        // Exercise 2 : add
+        // int age = 22;
+        // age = age + 1;
+        // System.out.println(age); output should be 23
+
+        // age = age + 1;
+        // System.out.println(age);
+
+        // output : 23
+    }
+}
